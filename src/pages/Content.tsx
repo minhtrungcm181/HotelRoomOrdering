c3a1d80c929ab38d449ade3e3e825c9e21a56668
Your Name <you@example.com>
2024-04-03 15:48:12 +0700

    homepage

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -4,10 +4,11 @@ const Content = () => {
     return (
         <div className="w-full lg:flex-1 bg-gray-600 mt-14">
             <main className="p-1 ml-72 pt-10">
-                
-       <Outlet/>
+            
+       
       </main>
         </div>
 
     )
 }
+export default Content;
